abe39b41f42322f3a6d065377da0112d1420b9d1
MyPoZi <kxx555u.u@gmail.com>
2019-07-18 18:03:05 +0900

    [add] guardとminitestをgemfileに追加し、設定を追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,7 +11,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
